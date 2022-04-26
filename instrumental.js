d17889a03f2b889aaded0815488a5d5a7c07a34f
Luiz Rodrigues <102743913+Luizera1@users.noreply.github.com>
2022-04-26 12:58:25 -0300

    Add files via upload

---
+++ instrumental.js
@@ -0,0 +1,34 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
+
+const swiper = new Swiper(`.swiper-container`, {
+  slidesPerView: 1,
+  pagination: {
+    el: `.swiper-pagination`
+  },
+  mousewheel: true,
+  keyboard: true
+})
